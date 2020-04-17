60f66a31a502d0753c77e93e9a687a172ced7b28
Sarisia <sarisiaice@gmail.com>
2020-04-17 14:03:36 +0900

    release: fix workflow & script

--- static/update.sh
+++ static/update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl -O https://github.com/aria-music/aria-discord-go/releases/latest/download/aria-discord-go-linux-amd64.zip
-unzip -o aria-discord-go-linux-amd64.zip
+curl -L -O https://github.com/aria-music/aria-discord-go/releases/latest/download/aria-discord-go-linux-amd64.zip
+unzip -o -j aria-discord-go-linux-amd64.zip
